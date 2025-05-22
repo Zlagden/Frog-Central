ec789f07a7d9065316dbd4b6639d77ed67d8458a
jewel campbell <jewelcampbell021@gmail.com>
2025-05-22 18:07:03 +0100

    a new index

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_22_105637) do
     t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "bookings", "frogs"
