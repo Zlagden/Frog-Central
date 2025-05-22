db793c0aea6461bc0641b790b3ec684ff24bd606
zakariya lagden <zlagden@gmail.com>
2025-05-22 12:38:04 +0100

    added photo-url and frogs to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_20_155044) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_22_105637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_20_155044) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
     t.index ["user_id"], name: "index_frogs_on_user_id"
   end
 
